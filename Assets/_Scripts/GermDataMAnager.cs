542de6fbb038ab3e3d451925c30c4d816e95b3f1
JerrySolis <98674534+JerrySolis@users.noreply.github.com>
2022-05-23 15:21:05 +0800

    set up almanac
    Assets>USsprites>Germ data pics> Germ Data Book.psd
    
    open mo lang to babe tas observe mo yung  mga layout

--- Assets/_Scripts/GermDataMAnager.cs
+++ Assets/_Scripts/GermDataMAnager.cs
@@ -11,7 +11,16 @@ public class GermDataMAnager : MonoBehaviour
                   GermData7, GermData8, GermData9,
                   GermData10, GermData11, GermData12,
                   GermData13, GermData14, GermData15,
-                  GermData16, GermData17, GermData18;
+                  GermData16, GermData17, GermData18,
+                  GermData19;
+    [SerializeField]
+    private GameObject nameGermData1, nameGermData2, nameGermData3,
+                       nameGermData4, nameGermData5, nameGermData6,
+                       nameGermData7, nameGermData8, nameGermData9,
+                       nameGermData10, nameGermData11, nameGermData12,
+                       nameGermData13, nameGermData14, nameGermData15,
+                       nameGermData16, nameGermData17, nameGermData18,
+                       nameGermData19;
     int dataStates = 0;
     void Awake()
     {
@@ -62,5 +71,85 @@ public class GermDataMAnager : MonoBehaviour
         GermData16.interactable = dataStates >= 16;
         GermData17.interactable = dataStates >= 17;
         GermData18.interactable = dataStates >= 18;
+        GermData19.interactable = dataStates >= 19;
+
+        if (GermData1.interactable)
+        {
+            nameGermData1.SetActive(true);
+        }
+        if (GermData2.interactable)
+        {
+            nameGermData2.SetActive(true);
+        }
+        if (GermData3.interactable)
+        {
+            nameGermData3.SetActive(true);
+        }
+        if (GermData4.interactable)
+        {
+            nameGermData4.SetActive(true);
+        }
+        if (GermData5.interactable)
+        {
+            nameGermData5.SetActive(true);
+        }
+        if (GermData6.interactable)
+        {
+            nameGermData6.SetActive(true);
+        }
+        if (GermData7.interactable)
+        {
+            nameGermData7.SetActive(true);
+        }
+        if (GermData8.interactable)
+        {
+            nameGermData8.SetActive(true);
+        }
+        if (GermData9.interactable)
+        {
+            nameGermData9.SetActive(true);
+        }
+        if (GermData10.interactable)
+        {
+            nameGermData10.SetActive(true);
+        }
+        if (GermData11.interactable)
+        {
+            nameGermData11.SetActive(true);
+        }
+        if (GermData12.interactable)
+        {
+            nameGermData12.SetActive(true);
+        }
+        if (GermData13.interactable)
+        {
+            nameGermData13.SetActive(true);
+        }
+        if (GermData14.interactable)
+        {
+            nameGermData14.SetActive(true);
+        }
+        if (GermData15.interactable)
+        {
+            nameGermData15.SetActive(true);
+        }
+        if (GermData16.interactable)
+        {
+            nameGermData16.SetActive(true);
+        }
+        if (GermData17.interactable)
+        {
+            nameGermData17.SetActive(true);
+        }
+        if (GermData18.interactable)
+        {
+            nameGermData18.SetActive(true);
+        }
+        if (GermData19.interactable)
+        {
+            nameGermData19.SetActive(true);
+        }
+
+
     }
 }
